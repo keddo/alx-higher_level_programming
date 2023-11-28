abfc244682de8f2b603a3846db3936f34c6dcd27
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-28 15:47:10 +0300

    fix error

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char in range(97, 123):
-    if chr(char) != 'q' and chr(char) != 'e':
-        print(chr(char), end='')
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
