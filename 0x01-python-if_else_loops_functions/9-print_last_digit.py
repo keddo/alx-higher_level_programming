315fcbb3f823c3a2e77250a1a7d446a2ccf3c7ae
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-28 15:54:48 +0300

    fix error

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     """Print the last digit of a number and return it"""
-    return (number % 10) if number > 0 else (abs(number) % 10)
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
