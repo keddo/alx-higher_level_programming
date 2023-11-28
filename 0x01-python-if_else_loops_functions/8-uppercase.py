aeb6987b12d3cef16d60078f609b60eecce62e2b
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-28 14:12:04 +0300

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper_str = ''
+    for char in str:
+        if char >= 'a' and char <= 'z':
+            upper_str += chr(ord(char) - 32)
+        else:
+            upper_str += char
+    print(upper_str)
