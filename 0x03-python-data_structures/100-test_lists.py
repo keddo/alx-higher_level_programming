fc280c9393e6f4e77e82198ab89d8f04dd55ab35
keddo <adu.kedir.abdurahman@gmail.com>
2023-12-04 09:45:21 +0300

    fix error

--- 0x03-python-data_structures/100-test_lists.py
+++ 0x03-python-data_structures/100-test_lists.py
@@ -2,20 +2,20 @@ import ctypes
 
 lib = ctypes.CDLL('./libPyList.so')
 lib.print_python_list_info.argtypes = [ctypes.py_object]
-l = ['hello', 'World']
-lib.print_python_list_info(l)
-del l[1]
-lib.print_python_list_info(l)
-l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "My string"]
-lib.print_python_list_info(l)
-l = []
-lib.print_python_list_info(l)
-l.append(0)
-lib.print_python_list_info(l)
-l.append(1)
-l.append(2)
-l.append(3)
-l.append(4)
-lib.print_python_list_info(l)
-l.pop()
-lib.print_python_list_info(l)
+ll = ['hello', 'World']
+lib.print_python_list_info(ll)
+del ll[1]
+lib.print_python_list_info(ll)
+ll = ll + [4, 5, 6.0, (9, 8), [9, 8, 1024], "My string"]
+lib.print_python_list_info(ll)
+m = []
+lib.print_python_list_info(m)
+m.append(0)
+lib.print_python_list_info(m)
+m.append(1)
+m.append(2)
+m.append(3)
+m.append(4)
+lib.print_python_list_info(m)
+m.pop()
+lib.print_python_list_info(m)
