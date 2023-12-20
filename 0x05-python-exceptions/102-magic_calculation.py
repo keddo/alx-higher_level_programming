e31021ab667db6694334a824bec0ee5eeaeac2ac
keddo <adu.kedir.abdurahman@gmail.com>
2023-12-20 07:20:29 +0300

    fix error

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,11 +3,12 @@ def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
-            if (i > a):
-                raise Exception("Too far")
+            if i > a:
+                raise Exception('Too far')
             else:
                 result += (a ** b) / i
         except:
             result = b + a
             break
-    return (result)
+    return result
+
