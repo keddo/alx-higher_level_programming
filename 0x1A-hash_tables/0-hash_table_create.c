4369817363711ef13b7da85b55d78dd3a7d3519a
keddo <adu.kedir.abdurahman@gmail.com>
2023-12-22 05:36:02 +0300

    fix'

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,22 +8,22 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    unsigned long int i;
-    hash_table_t *hashtable;
+unsigned long int i;
+hash_table_t *hashtable;
 
-    if (size == 0)
-        return (NULL);
-    hashtable = malloc(sizeof(hash_table_t));
-    if (hashtable == NULL)
-        return (NULL);
-    hashtable->size = size;
-    hashtable->array = malloc(sizeof(hash_node_t *) * size);
-    if (hashtable->array == NULL)
-        return (NULL);
+if (size == 0)
+return (NULL);
+hashtable = malloc(sizeof(hash_table_t));
+if (hashtable == NULL)
+return (NULL);
+hashtable->size = size;
+hashtable->array = malloc(sizeof(hash_node_t *) * size);
+if (hashtable->array == NULL)
+return (NULL);
 
-    for (i = 0; i < size; i++)
-    {
-        hashtable->array[i] = NULL;
-    }
-    return (hashtable);
+for (i = 0; i < size; i++)
+{
+hashtable->array[i] = NULL;
+}
+return (hashtable);
 }
